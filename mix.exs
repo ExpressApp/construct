50ad37ad7fc17cce3fc038b4d81e2bad9711eb22
Yuri Artemev <i@artemeff.com>
2016-12-22 12:51:53 +0300

    bump version to 0.1.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Struct.Mixfile do
 
   def project do
     [app: :struct,
-     version: "0.1.0",
+     version: "0.1.1",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
