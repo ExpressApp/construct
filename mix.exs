21ecaf58fb90e97980e73862fddc8f58ce9830f3
Yuri Artemev <i@artemeff.com>
2023-04-11 12:43:30 +0300

    release 3.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Construct.Mixfile do
   def project do
     [
       app: :construct,
-      version: "3.0.0",
+      version: "3.0.1",
       elixir: "~> 1.9",
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env),
